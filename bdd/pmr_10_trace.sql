7ef80a6538c2ede7a3e96df739a02afd809abe4b
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-10-09 11:37:54 +0200

    Create pmr_10_trace.sql

---
+++ bdd/pmr_10_trace.sql
@@ -0,0 +1,12 @@
+/*pmr V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* pmr_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+
+-- 2023/10/09 : GB / Initialisation du code SQL pour la création de structure de la base de données
